960a63cbb24b93e66c0ccf4f01330da960a11223
M. Rizky Kurniawan Lasim <153719279+RIZKY07606@users.noreply.github.com>
2025-05-20 23:56:27 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,52 +1,46 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"ukk-smkn2/database"
 	"ukk-smkn2/docs"
 	"ukk-smkn2/routes"
-
-	"os"
 	e "ukk-smkn2/entities"
 
-	_ "ukk-smkn2/docs" // for Swagger docs
+	_ "ukk-smkn2/docs"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/swagger"
 )
 
-//	@title			API UKK SMKN2 SURABAYA DOCUMENTATION
-//	@version		1.0
-//	@description	Documentation for API UKK SMKN2 Surabaya
-//	@host			127.0.0.1:8080
-//	@BasePath		/
-
-// @securityDefinitions.apikey BearerAuth
-// @in header
-// @name Authorization
 func main() {
-	if os.Getenv("ENV") != "production" {
-		docs.SwaggerInfo.Host = "127.0.0.1:8080"
-	} else {
-		docs.SwaggerInfo.Host = "ukk-3738-hxpe9rkh.leapcell.dev" // change later
+	host := os.Getenv("SWAGGER_HOST")
+	if host == "" {
+		host = "127.0.0.1:8080"
 	}
+	docs.SwaggerInfo.Host = host
 
 	app := fiber.New(fiber.Config{
 		EnablePrintRoutes: true,
 	})
 	app.Use(logger.New())
-	database.Connect()
-	db := database.DB
-	// db.AutoMigrate(&e.User{}) //migrate later
-	// db.AutoMigrate(&e.User{}, &e.otherEntities{})
-	db.AutoMigrate(&e.Admin{})
-	db.AutoMigrate(&e.Siswa{})
-	db.AutoMigrate(&e.KategoriKarya{})
-	db.AutoMigrate(&e.KaryaUKK{})
-	db.AutoMigrate(&e.FileUpload{})
-	db.AutoMigrate(&e.Review{})
-	routes.SetupRoutes(app, db)
-	app.Get("/swagger/*", swagger.HandlerDefault)
 
-	app.Listen(":8080")
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatalf("❌ Database connection failed: %v", err)
+	}
+
+	if err := db.AutoMigrate(
+		&e.Admin{},
+		&e.Siswa{},
+		&e.KategoriKarya{},
+		&e.KaryaUKK{},
+		&e.FileUpload{},
+		&e.Review{},
+	); err != nil {
+		log.Fatalf("❌ AutoMigrate failed: %v", err)
+	}
 }
